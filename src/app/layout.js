a76823dd215d8ebef13f9f334bb2b2157439132d
fareaz <fareazuddin22@gmail.com>
2026-01-13 20:13:29 +0600

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata = {
+  title: "Items App",
+  description: "Simple Next.js Items Application",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
